3fe7d67f81895216d59f18bd0652ee2fba4e1326
lesaphil1150 <lesaphil1150@gmail.com>
2021-09-04 10:34:08 -0400

    finished auth, login, and sign up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import Auth from "./auth/Auth";
 
 function App() {
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken("");
+  };
+
+  // let protectedViews = () => {
+  //   return sessionToken === localStorage.getItem("token") ? (
+  //     <InventoryIndex token={sessionToken} clearToken={clearToken} />
+  //   ) : (
+  //   );
+  // };
   return (
     <div>
-    
+      <Auth updateToken={updateToken} />;{/* <Signup /> */}
+      <Login updateToken={updateToken} />
+      {/* {protectedViews()} */}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,1 +1,59 @@
-/* delete me pls */
+import React, { useState } from "react";
+import { Form, Label, Input, Button, FormGroup } from "reactstrap";
+
+const Signup = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/user/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        user: { email: email, password: password, name: name },
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.updateToken(data.sessionToken);
+      });
+  };
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            value={name}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={email}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />
+        </FormGroup>
+        <Button type="submit">Sign up</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
