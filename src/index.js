c4fd1d6a7f8da8ed5e98ab703399266a1ae89569
jcharfauros <julia.charfauros@gmail.com>
2021-09-06 00:15:36 -0400

    Client - InventoryEdit.jsx code added, need to test on Postman. Also added edit code into InventoryIndex.jsx. Fixed line 1 in index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-//no longer being used in current bootstrap version - julia
-// import 'bootstrap/dis/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
