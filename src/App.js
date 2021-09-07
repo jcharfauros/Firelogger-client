df5a76a19c89dff452eb0aa78a77d8a45ef8c101
Stephanie Falls <stephms@gmail.com>
2021-09-07 18:09:41 -0400

    InvetoryIndex first draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Sitebar from "./home/Navbar";
+import Auth from "./auth/Auth";
+import InventoryIndex from "./inventory/InventoryIndex";
 
 function App() {
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+
+  const protectedViews = () => {
+    return sessionToken === localStorage.getItem("token") ? (
+      <InventoryIndex token={sessionToken} />
+    ) : (
+      <Auth updateToken={updateToken} />
+    );
+  };
+
   return (
     <div>
-    test
+      <Sitebar clickLogout={clearToken} />
+      {protectedViews()}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/inventory/InventoryIndex.jsx
+++ src/inventory/InventoryIndex.jsx
@@ -1,1 +1,77 @@
-/* delete me */
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import InventoryCreate from "./InventoryCreate";
+import InventoryTable from "./InventoryTable";
+import InventoryEdit from "./InventoryEdit";
+
+const InventoryIndex = (props) => {
+  const [inventory, setInventory] = useState([]);
+  const [editActive, setEditActive] = useState(false);
+  const [inventoryToEdit, setInventoryToEdit] = useState({});
+
+  const fetchInventory = () => {
+    fetch("http://localhost:3000/inventory/", {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorizaiton: "props.token",
+      }),
+    })
+      .then((res) => res.json())
+      .then((inventoryData) => {
+        setInventory(inventoryData);
+        console.log(inventoryData);
+      });
+  };
+
+  const editInventory = (inventory) => {
+    setInventoryToEdit(inventory);
+    console.log(inventory);
+  };
+
+  const editOn = () => {
+    setEditActive(true);
+  };
+
+  const editOff = () => {
+    setEditActive(false);
+  };
+
+  useEffect(() => {
+    fetchInventory();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col md="3">
+          <InventoryCreate
+            fetchInventory={fetchInventory}
+            token={props.token}
+          />
+        </Col>
+        <Col md="9">
+          <InventoryTable
+            inventory={inventory}
+            editInventory={editInventory}
+            editOn={editOn}
+            fetchInventory={fetchInventory}
+            token={props.token}
+          />
+        </Col>
+        {editActive ? (
+          <InventoryEdit
+            inventoryToEdit={inventoryToEdit}
+            editOff={editOff}
+            token={props.token}
+            fetchInventory={fetchInventory}
+          />
+        ) : (
+          <></>
+        )}
+      </Row>
+    </Container>
+  );
+};
+
+export default InventoryIndex;
