ef4236298f4d48eef91ab4b5961929807e180af8
lesaphil1150 <lesaphil1150@gmail.com>
2021-09-12 20:43:11 -0400

    Leslie's update, adding Google Maps(Hotel and Pet) API Fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 import InventoryIndex from "./inventory/InventoryIndex";
 import Auth from "./auth/Auth";
 import FireloggerNavbar from "./home/Navbar";
 import Login from "./auth/Login";
+import Resources from "./home/Resources";
+import Hotels from "./home/Hotels";
+import Pets from "./home/Pet";
 
 function App() {
   const [sessionToken, setSessionToken] = useState("");
@@ -34,10 +38,11 @@ function App() {
 
   return (
     <div>
-      {/* <Auth updateToken={updateToken} /> */}
       <FireloggerNavbar clickLogout={clearToken} />
-      <Login updateToken={updateToken} />
+
       {protectedViews()}
+      <Hotels />
+      <Pets />
     </div>
   );
 }
---
+++ src/home/Hotels.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { Row, Container, Table, Button } from "reactstrap";
+import ReactHtmlParser from "react-html-parser";
+
+const Hotels = (props) => {
+  const [hotel, setHotel] = useState({});
+
+  let hotelfetch = (e) => {
+    fetch("http://localhost:3000/hotels", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setHotel(data.results);
+        console.log(data.results);
+      });
+  };
+
+  useEffect(() => {
+    hotelfetch();
+  }, []);
+
+  const hotelMapper = () => {
+    return hotel.map((hotel) => {
+      return (
+        <tr key={hotel.id}>
+          <td>{hotel.name}</td>
+          <td>{hotel.formatted_address}</td>
+          <td>
+            {ReactHtmlParser(hotel.photos[0].html_attributions)}
+            {/* <a
+              href={ReactHtmlParser(hotel.photos[0].html_attributions)}
+              target="_blank"
+            >
+              <button>Google Map Link</button>
+            </a> */}
+            {/* <a
+              id="googleLink"
+              href={ReactHtmlParser(hotel.photos[0].html_attributions)}
+              target="_blank"
+            >
+              Google Map Link
+            </a> */}
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <h2 className="hotel_title">Hotels Nearby</h2>
+        <hr />
+        <div className="hotelTable">
+          <Table striped>
+            <thead>
+              <tr>
+                <th>Name:</th>
+                <th>Address:</th>
+                <th>Link to Google Map Location:</th>
+              </tr>
+            </thead>
+            <tbody>{hotel.length > 0 ? hotelMapper() : "Loading"}</tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hotels;
