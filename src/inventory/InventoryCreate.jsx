ce197fe456fb7ce372bdc8e532d36f563ef367ef
lesaphil1150 <lesaphil1150@gmail.com>
2021-09-23 16:17:21 -0400

    updated inventory to add validation

--- src/inventory/InventoryCreate.jsx
+++ src/inventory/InventoryCreate.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useFormik } from "formik"; //import Formik
+import * as Yup from "yup";
 import "../App.css";
 import {
   Button,
@@ -11,19 +13,46 @@ import {
   ModalHeader,
 } from "reactstrap";
 
-  const InventoryCreate = (props) => {
-  const [category, setCategory] = useState("");
-  const [name, setName] = useState("");
-  const [year, setYear] = useState("");
-  const [model, setModel] = useState("");
-  const [serial_number, setSerial_Number] = useState("");
-  const [pic_url, setPic_Url] = useState("");
-  const [value, setValue] = useState("");
+const InventoryCreate = (props) => {
+  // const [category, setCategory] = useState("");
+  // const [name, setName] = useState("");
+  // const [year, setYear] = useState("");
+  // const [model, setModel] = useState("");
+  // const [serial_number, setSerial_Number] = useState("");
+  // const [pic_url, setPic_Url] = useState("");
+  // const [value, setValue] = useState("");
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
   const handleClose = () => setModal(false);
+
+  const formik = useFormik({
+    initialValues: {
+      category: "",
+      name: "",
+      year: "",
+      model: "",
+      serial_number: "",
+      value: "",
+      // image: "",
+    },
+    validationSchema: Yup.object({
+      category: Yup.string().required("Required"),
+      name: Yup.string().required("Required"),
+      year: Yup.string().required("Required"),
+      model: Yup.string().required("Required"),
+      serial_number: Yup.string().required("Required"),
+      value: Yup.string().required("Required"),
+      // pic_url: Yup.string(),
+    }),
+
+    onSubmit: (values) => {
+      console.log("hello");
+      postInventory();
+    },
+  });
+
   let UploadImage = async (e) => {
     let files = e.target.files;
     let data = new FormData();
@@ -41,18 +70,18 @@ import {
     setImage(File.secure_url);
     setLoading(false);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const postInventory = (e) => {
+    // e.preventDefault();
     fetch("http://localhost:3000/inventory/create", {
       method: "POST",
       body: JSON.stringify({
         inventory: {
-          category: category,
-          name: name,
-          year: year,
-          model: model,
-          serial_number: serial_number,
-          value: value,
+          category: formik.values.category,
+          name: formik.values.name,
+          year: formik.values.year,
+          model: formik.values.model,
+          serial_number: formik.values.serial_number,
+          value: formik.values.value,
           pic_url: image,
         },
       }),
@@ -63,25 +92,15 @@ import {
     })
       .then((res) => res.json())
       .then((inventoryData) => {
-        console.log(inventoryData);
-        setCategory("");
-        setName("");
-        setYear("");
-        setModel("");
-        setSerial_Number("");
-        setValue("");
-        setPic_Url("");
-        props.fetchInventory();
+        window.location.href = "/";
+        // // props.fetchInventory();
+        // handleClose();
       });
   };
+
   return (
     <div>
-      <Button
-        variant="primary"
-        onClick={toggle}
-        // className="add-inventory-button"
-        className='btn-pdf'
-      >
+      <Button variant="primary" onClick={toggle} className="btn-pdf">
         Add item to Inventory
       </Button>
       <Modal isOpen={modal} toggle={toggle}>
@@ -89,14 +108,16 @@ import {
           <h3>Add item to Inventory</h3>
         </ModalHeader>
         <ModalBody>
-          <Form onSubmit={handleSubmit}>
+          {/* should be handle submit here around time 17min*/}
+          <Form onSubmit={formik.handleSubmit}>
             <FormGroup>
               <Label htmlFor="category" />
               <Input
                 type="select"
                 name="category"
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
+                value={formik.values.category}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               >
                 <option>Category of Item</option>
                 <option value="electronics">Electronics</option>
@@ -106,51 +127,86 @@ import {
                 <option value="antiques">Antiques</option>
                 <option value="other/misc">Other/Misc</option>
               </Input>
+              <p style={{ color: "red" }}>
+                {formik.touched.category && formik.errors.category ? (
+                  <div>{formik.errors.category}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="name" />
               <Input
                 name="name"
                 placeholder="Name of Item"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              <p style={{ color: "red" }}>
+                {formik.touched.name && formik.errors.name ? (
+                  <div>{formik.errors.name}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="year" />
               <Input
                 name="year"
                 placeholder="Year Purchased"
-                value={year}
-                onChange={(e) => setYear(e.target.value)}
+                value={formik.values.year}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              <p style={{ color: "red" }}>
+                {formik.touched.year && formik.errors.year ? (
+                  <div>{formik.errors.year}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="model" />
               <Input
                 name="model"
                 placeholder="Model of Item"
-                value={model}
-                onChange={(e) => setModel(e.target.value)}
+                value={formik.values.model}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              <p style={{ color: "red" }}>
+                {formik.touched.model && formik.errors.model ? (
+                  <div>{formik.errors.model}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="serial_number" />
               <Input
                 name="serial_number"
                 placeholder="Serial Number"
-                value={serial_number}
-                onChange={(e) => setSerial_Number(e.target.value)}
+                value={formik.values.serial_number}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              <p style={{ color: "red" }}>
+                {formik.touched.serial_number && formik.errors.serial_number ? (
+                  <div>{formik.errors.serial_number}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="value" />
               <Input
                 name="value"
                 placeholder="Value of Item"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
+                value={formik.values.value}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              <p style={{ color: "red" }}>
+                {formik.touched.value && formik.errors.value ? (
+                  <div>{formik.errors.value}</div>
+                ) : null}
+              </p>
             </FormGroup>
             <FormGroup>
               <Label htmlFor="pic_url" />
@@ -160,6 +216,8 @@ import {
                 name="file"
                 placeholder="Upload Image here"
                 onChange={UploadImage}
+                // onChange={handleChange}
+                // onBlur={formik.handleBlur}
               />
               <br />
               {loading ? (
@@ -169,12 +227,7 @@ import {
               )}
             </FormGroup>{" "}
             <br />
-            <Button type="submit" onClick={handleClose}>
-              Click to Add Item
-            </Button>{" "}
-            {/* <Button color="secondary" onClick={toggle}>
-              Cancel
-            </Button> */}
+            <Button type="submit">Click to Add Item</Button>{" "}
           </Form>
         </ModalBody>
       </Modal>
